bc3968ea3f3e4d9e09bfb644ab748407e3d07644
Kiran gavini <kirangavini1993@gmail.com>
2017-01-08 23:17:08 -0600

    updated language

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = 3000;
 
 var middleware = {
       requireAunthentication: function (req, res, next) {
-      	console.log('private route hit');
+      	console.log('private rout!e hit');
       	next();
       },
       logger: function (req, res, next) {
