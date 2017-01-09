85136105c38285caa1610cd22e8ebd35cc7e0332
Kiran gavini <kirangavini1993@gmail.com>
2017-01-08 23:14:59 -0600

    Intialise repo

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app =express();
+var PORT = 3000;
+
+var middleware = {
+      requireAunthentication: function (req, res, next) {
+      	console.log('private route hit');
+      	next();
+      },
+      logger: function (req, res, next) {
+      	// new Date().toString()
+      	console.log('request' + new Date().toString() +req.method+ ' '+ req.originalUrl);
+      	next();
+      } 
+};
+
+// app.use(middleware.requireAunthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAunthentication, function (req, res) {
+     res.send('Hello Express');
+
+});
+
+/// about
+
+// about us
+app.get('/about', function (req, res) {
+     res.send('About Us');
+
+});
+
+ app.use(express.static(__dirname+ '/public'))
+console.log(__dirname);
+app.listen(PORT, function () {
+	console.log('Express server started on port' )
+});
